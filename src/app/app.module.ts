fd6c1626fb3382c669509e8d172b3f796e258253
Ryan Matos <ryan.matos@itixti.com.br>
2020-12-16 15:19:27 -0300

    Navbar modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AffComponent } from './aff/aff.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, AffComponent, NavbarComponent],
+  declarations: [AppComponent, AffComponent, NavbarComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot([{ path: 'aff', component: AffComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'aff', component: AffComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
